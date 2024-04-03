7e50d0689e11d43fd3bcd0b1344b83f5e87695bd
suhailak22 <139833962+suhailak22@users.noreply.github.com>
2024-04-03 10:26:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,26 @@
 provider "google" {
   credentials = file("tera.json")
-  project     = "cogent-theater-412814"
+  project     = "able-river-419007"
   region      = "us-central1"
 }
 
-terraform {
-  backend "gcs" {
-    bucket  = "alb234"
-    prefix  = "terraform/state"
-  }
-}
-
 resource "google_compute_instance" "demo-instance" {
-  name         = "demo-instance-3"
+  name         = "demo-instance-1"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-2004-focal-arm64-v20240307b"
+      image = "debian-cloud/debian-10"
     }
   }
 
-  // Specify the network interface configuration
   network_interface {
     network = "default"
-
-    // Access configuration
     access_config {
       // Ephemeral IP
     }
   }
-}
 
-resource "google_compute_firewall" "allow-ssh" {
-  name    = "allow-ssh"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "80"]
-  }
-
-  source_ranges = ["35.235.240.0/20"]  # Allow traffic from any IP address
+  metadata_startup_script = "echo 'Hello, World!' > /var/www/html/index.html && sudo service apache2 restart"
 }
